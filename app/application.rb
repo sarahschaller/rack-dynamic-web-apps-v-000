d5e1bd303c62ac8dbe2c5181678366ff3b0c152d
Sarah Schaller <sarcsch@aol.com>
2017-09-21 13:41:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,9 @@ class Application
 
     if num_1 == num_2 && num_2 == num_3
       resp.write "You Win"
-    else 
+    else
       resp.write "You Lose"
-    end 
+    end
 
     resp.finish
   end
